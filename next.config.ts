2c992806954bcc76874ac8ba4f71b13cb5edd4cb
pota2dev <mahdihasan535@gmail.com>
2025-05-15 02:53:12 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Warning: This will ignore *all* lint errors during Vercel builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
